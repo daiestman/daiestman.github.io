f458ecd9e6f861df3b7021ea2bae4b503e8745fb
daiest <2741830473@qq.com>
2025-01-29 01:30:36 +0800

    Site updated: 2025-01-29 01:30:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/27/hello-world/","2025/01/28/好用的手表QQ＆修改版/"];function toRandomPost(){
+var posts=["2025/01/27/hello-world/","2025/01/28/好用的手表QQ＆修改版/","2025/01/29/零成本建站之hexo前言/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
